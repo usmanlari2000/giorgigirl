cf6313cf68b904d97d406ab9410fd316ef142cc0
usmanlari2000 <usmanlari2000@gmail.com>
2024-11-09 11:36:42 +0500

    minor changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export default function Layout({ children }) {
   return (
